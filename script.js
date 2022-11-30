9539d9e268ba7404812ea6a16813b9effd38380f
louistam888 <louiscodes888@gmail.com>
2022-11-29 19:18:59 -0500

    got elevator outages fetch working

--- script.js
+++ script.js
@@ -1,28 +1,82 @@
 const app = {};
 
 app.token = "J33yX1FYA0vwnOA36tGBFLd6l"
+app.elevatorEscalatorURL = "https://data.edmonton.ca/resource/snws-u3zx.json"
 
-app.urlElevator = "https://data.edmonton.ca/resource/snws-u3zx.json?device_type=Elevator"
-app.urlEscalator = "https://data.edmonton.ca/resource/snws-u3zx.json?device_type=Escalator"
+const convertTime = (timeObj) => { 
+  return new Date(timeObj).toLocaleDateString("en-us", {
+    year: "numeric",
+    month: "short",
+    weekday: "long",
+    day: "numeric",
+    hour12: true,
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit"
+  })
+}
 
-// const currentTime = (time) => {
-//   const convertedTime = time.toLocaleString('en-CA', {timeZone: "America/Edmonton", year:"numeric", month:"short", weekday: "long", day:"numeric", hour12:true, hour: "numeric", minute:"2-digit", second: "2-digit"});
-//   return convertedTime;
-// }
-
-// currentTime()
-// setInterval(currentTime, 1000);
-
-
-const refreshTime = () => {
+//FUNCTION FOR RETRIEVING AND UPDATING CURRENT EDMONTON TIME 
+app.refreshTime = () => {
   const timeNow = (new Date).toLocaleString('en-CA', {timeZone: "America/Edmonton", year:"numeric", month:"short", weekday: "long", day:"numeric", hour12:true, hour: "numeric", minute:"2-digit", second: "2-digit"})
 
   const time = document.querySelector(".time")
   time.innerHTML = `${timeNow}`
 }
-
-refreshTime()
-setInterval(refreshTime, 1000)
-
+app.refreshTime()
+setInterval(app.refreshTime, 1000)
 
 
+app.fetchElevator = () => {
+  
+  const url = "https://data.edmonton.ca/resource/snws-u3zx.json?device_type=Elevator"
+
+  url.search = new URLSearchParams ({
+    "$$app_token": app.token
+  });
+
+  fetch(url)
+    .then((result) => {
+      return result.json()
+    })
+    .then((data)=> {
+
+      document.querySelector(".elevatorOutages").replaceChildren();
+
+      data.forEach((elevatorObj)=>{
+        
+        const stationName = elevatorObj.lrt_station_name;
+        const deviceLocation = elevatorObj.lrt_device_location 
+        const device_op_status = elevatorObj.device_op_status;
+        const offlineStartDate = convertTime(elevatorObj.since_time_stamp);
+        const lastUpdated = convertTime(elevatorObj.polled_time_stamp);
+
+        const newLi = document.createElement("li");
+        newLi.classList.add("stationGrid");
+        newLi.innerHTML = 
+
+            `<div> 
+              ${stationName}
+            </div>
+            <div> 
+              ${deviceLocation}
+            </div>
+            <div> 
+              ${device_op_status}
+            </div>
+            <div> 
+              ${offlineStartDate}
+            </div>
+            <div> 
+              ${lastUpdated}
+            </div>`
+        
+        document.querySelector(".elevatorOutages").append(newLi)
+
+      })
+    })
+  }
+
+
+app.fetchElevator()
+setInterval(app.fetchElevator, 300000);
