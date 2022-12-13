cdd317b2a66f2bdcf5de55fceddcfd0f0c94a52a
louistam888 <louiscodes888@gmail.com>
2022-12-13 15:01:21 -0500

    added escalator data

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const app = {};
 
 app.token = "J33yX1FYA0vwnOA36tGBFLd6l"
-app.elevatorEscalatorURL = "https://data.edmonton.ca/resource/snws-u3zx.json"
+app.escalatorEscalatorURL = "https://data.edmonton.ca/resource/snws-u3zx.json"
 
 const convertTime = (timeObj) => { 
   return new Date(timeObj).toLocaleDateString("en-us", {
@@ -27,6 +27,8 @@ app.refreshTime()
 setInterval(app.refreshTime, 1000)
 
 
+
+//FUNCTION FOR RETRIEVING AND UPDATING ESCALTOR OUTAGES EVERY FIVE MINUTES 
 app.fetchElevator = () => {
   
   const url = "https://data.edmonton.ca/resource/snws-u3zx.json?device_type=Elevator"
@@ -77,6 +79,68 @@ app.fetchElevator = () => {
     })
   }
 
-
 app.fetchElevator()
 setInterval(app.fetchElevator, 300000);
+
+
+
+
+
+
+
+
+
+//FUNCTION FOR RETRIEVING AND UPDATING ELEVATOR OUTAGES EVERY FIVE MINUTES 
+app.fetchEscalator = () => {
+  
+  const url = "https://data.edmonton.ca/resource/snws-u3zx.json?device_type=Escalator"
+
+  url.search = new URLSearchParams ({
+    "$$app_token": app.token
+  });
+
+  fetch(url)
+    .then((result) => {
+      return result.json()
+    })
+    .then((data)=> {
+
+      document.querySelector(".escalatorOutages").replaceChildren();
+
+      data.forEach((escalatorObj)=>{
+        
+        const stationName = escalatorObj.lrt_station_name;
+        const deviceLocation = escalatorObj.lrt_device_location 
+        const device_op_status = escalatorObj.device_op_status;
+        const offlineStartDate = convertTime(escalatorObj.since_time_stamp);
+        const lastUpdated = convertTime(escalatorObj.polled_time_stamp);
+
+        const newLi = document.createElement("li");
+        newLi.classList.add("stationGrid");
+        newLi.innerHTML = 
+
+            `<div> 
+              ${stationName}
+            </div>
+            <div> 
+              ${deviceLocation}
+            </div>
+            <div> 
+              ${device_op_status}
+            </div>
+            <div> 
+              ${offlineStartDate}
+            </div>
+            <div> 
+              ${lastUpdated}
+            </div>`
+        
+        document.querySelector(".escalatorOutages").append(newLi)
+      })
+    })
+  }
+
+app.fetchEscalator()
+setInterval(app.fetchEscalator, 300000);
+
+
