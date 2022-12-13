46187616ff6d2387b583ddd1f7a67541f7cd5787
louistam888 <louiscodes888@gmail.com>
2022-12-13 18:46:14 -0500

    added error handling

--- script.js
+++ script.js
@@ -28,35 +28,80 @@ setInterval(app.refreshTime, 1000)
 
 
 
-//FUNCTION FOR RETRIEVING AND UPDATING ESCALTOR OUTAGES EVERY FIVE MINUTES 
-app.fetchElevator = () => {
+//FUNCTION FOR FETCHING ELEVATOR OUTAGES 
+
+app.fetchElevator = async () => {
+  try {
+    const url = "https://data.edmonton.ca/resource/snws-u3zx.json?device_type=Elevator"
+    url.search = new URLSearchParams ({
+      "$$app_token": app.token
+    });
+    
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+
+  } catch (error) {
+
+    document.querySelector(".elevatorOutages").replaceChildren();
+    const newLi = document.createElement("li");
+    newLi.classList.add("apiError");
+    newLi.innerHTML = `Sorry, information on elevator outages is not available at the moment. Please try again later`
+    document.querySelector(".elevatorOutages").append(newLi)
+
+  return error
+  }
+}
   
-  const url = "https://data.edmonton.ca/resource/snws-u3zx.json?device_type=Elevator"
+//FUNCTION FOR FETCHING ESCALATOR OUTAGES 
 
-  url.search = new URLSearchParams ({
-    "$$app_token": app.token
-  });
+app.fetchEscalator = async () => {
+  try {
+    const url = "https://data.edmonton.ca/resource/snws-u3zx.json?device_type=Escalator"
+    url.search = new URLSearchParams ({
+      "$$app_token": app.token
+    });
+    
+    const response = await fetch(url)
+    const data = await response.json();
+    return data;
 
-  fetch(url)
-    .then((result) => {
-      return result.json()
-    })
-    .then((data)=> {
+  } catch (error) {
+    
+    document.querySelector(".escalatorOutages").replaceChildren();
+    const newLi = document.createElement("li");
+    newLi.classList.add("apiError");
+    newLi.innerHTML = `Sorry, information on escalator outages is not available at the moment. Please try again later`
+    document.querySelector(".escalatorOutages").append(newLi)
+    
+    return error
+  }
+}
 
+const callAllPromises = () => { 
+
+  app.fetchElevator().then((elevator)=> {
+    if (elevator.length === 0) {
       document.querySelector(".elevatorOutages").replaceChildren();
+      const newLi = document.createElement("li");
+      newLi.classList.add("apiError");
+      newLi.innerHTML = `All elevators are currently operational`
+      document.querySelector(".elevatorOutages").append(newLi)
 
-      data.forEach((elevatorObj)=>{
-        
+    } else {
+
+      elevator.forEach((elevatorObj)=>{
+          
         const stationName = elevatorObj.lrt_station_name;
         const deviceLocation = elevatorObj.lrt_device_location 
         const device_op_status = elevatorObj.device_op_status;
         const offlineStartDate = convertTime(elevatorObj.since_time_stamp);
         const lastUpdated = convertTime(elevatorObj.polled_time_stamp);
-
+    
         const newLi = document.createElement("li");
         newLi.classList.add("stationGrid");
         newLi.innerHTML = 
-
+    
             `<div> 
               ${stationName}
             </div>
@@ -74,51 +119,32 @@ app.fetchElevator = () => {
             </div>`
         
         document.querySelector(".elevatorOutages").append(newLi)
-
       })
-    })
-  }
+    }
+  })
 
-app.fetchElevator()
-setInterval(app.fetchElevator, 300000);
-
-
-
-
-
-
-
-
-
-//FUNCTION FOR RETRIEVING AND UPDATING ELEVATOR OUTAGES EVERY FIVE MINUTES 
-app.fetchEscalator = () => {
-  
-  const url = "https://data.edmonton.ca/resource/snws-u3zx.json?device_type=Escalator"
-
-  url.search = new URLSearchParams ({
-    "$$app_token": app.token
-  });
-
-  fetch(url)
-    .then((result) => {
-      return result.json()
-    })
-    .then((data)=> {
-
+  app.fetchEscalator().then((escalator)=> {
+    if (escalator.length === 0) {
       document.querySelector(".escalatorOutages").replaceChildren();
-
-      data.forEach((escalatorObj)=>{
-        
+      const newLi = document.createElement("li");
+      newLi.classList.add("apiError");
+      newLi.innerHTML = `All escalators are currently operational`
+      document.querySelector(".escalatorOutages").append(newLi)
+  
+    } else {
+  
+      escalator.forEach((escalatorObj)=>{
+          
         const stationName = escalatorObj.lrt_station_name;
         const deviceLocation = escalatorObj.lrt_device_location 
         const device_op_status = escalatorObj.device_op_status;
         const offlineStartDate = convertTime(escalatorObj.since_time_stamp);
         const lastUpdated = convertTime(escalatorObj.polled_time_stamp);
-
+    
         const newLi = document.createElement("li");
         newLi.classList.add("stationGrid");
         newLi.innerHTML = 
-
+    
             `<div> 
               ${stationName}
             </div>
@@ -134,13 +160,18 @@ app.fetchEscalator = () => {
             <div> 
               ${lastUpdated}
             </div>`
-        
+
         document.querySelector(".escalatorOutages").append(newLi)
       })
-    })
-  }
+    }
+  })
+}
 
-app.fetchEscalator()
-setInterval(app.fetchEscalator, 300000);
+callAllPromises()
+setInterval(callAllPromises, 300000)
+
+
+
+
 
 
