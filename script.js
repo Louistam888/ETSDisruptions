17852c01b2c73187dc110e58539956d4d8617181
louistam888 <louiscodes888@gmail.com>
2023-01-04 18:48:20 -0500

    regex edited time in header

--- script.js
+++ script.js
@@ -19,7 +19,10 @@ const convertTime = (timeObj) => {
 
 //FUNCTION FOR RETRIEVING AND UPDATING CURRENT EDMONTON TIME 
 app.refreshTime = () => {
-  const timeNow = (new Date).toLocaleString('en-CA', {timeZone: "America/Edmonton", year:"numeric", month:"short", weekday: "long", day:"numeric", hour12:true, hour: "numeric", minute:"2-digit", second: "2-digit"})
+
+  let t = 0
+
+  const timeNow = (new Date).toLocaleString('en-CA', {timeZone: "America/Edmonton", year:"numeric", month:"short", weekday: "long", day:"numeric", hour12:true, hour: "numeric", minute:"2-digit", second: "2-digit"}).replace(/,/g, match => ++t === 3 ? ' |' : match)
 
   const time = document.querySelector(".time")
   time.innerHTML = `${timeNow}`
