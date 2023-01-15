fe8b7775cfd923027b2ec107f43b10e073cf9113
louistam888 <louiscodes888@gmail.com>
2023-01-15 17:16:52 -0500

    reverted old time code

--- javascript/script.js
+++ javascript/script.js
@@ -3,7 +3,7 @@ const app = {};
 app.token = "J33yX1FYA0vwnOA36tGBFLd6l"
 
 const convertTime = (timeObj) => { 
-  const dateString = new Date(timeObj).toLocaleDateString("en-us", {
+  return new Date(timeObj).toLocaleDateString("en-us", {
     year: "numeric",
     month: "short",
     weekday: "short",
@@ -11,22 +11,8 @@ const convertTime = (timeObj) => {
     hour12: true,
     hour: "numeric",
     minute: "2-digit"
-  }).split(",")
-
-
-
-  const weekday = dateString[0];
-  const monthDay = dateString[1];
-  const year = dateString[2];
-  const time = dateString[3];
-
-  const timeString = `${weekday}. ${monthDay}, ${year} @ ${time}`  
-  
-  return timeString
+  })
 }
-
-
-
 
 //FUNCTION FOR FETCHING ALL DISRUPTIONS
 
