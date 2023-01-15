1b61fbad31f43182b9b1bbaace6e7abbd2c63cc5
louistam888 <louiscodes888@gmail.com>
2023-01-15 16:07:34 -0500

    changed clock code

--- javascript/clock.js
+++ javascript/clock.js
@@ -2,9 +2,21 @@
 
 const refreshTime = () => {
 
-  let t = 0;
+  // let t = 0;
 
-  const timeNow = (new Date).toLocaleString('en-CA', {timeZone: "America/Edmonton", year:"numeric", month:"short", weekday: "long", day:"numeric", hour12:true, hour: "numeric", minute:"2-digit", second: "2-digit"}).toString().replace(/,/g, match => ++t === 3 ? ' |' : match)
+  // const timeNow = (new Date).toLocaleString('en-CA', {timeZone: "America/Edmonton", year:"numeric", month:"short", weekday: "long", day:"numeric", hour12:true, hour: "numeric", minute:"2-digit", second: "2-digit"}).toString().replace(/,/g, match => ++t === 3 ? ' |' : match)
+  
+  const date = new Date();
+  const dateString = date.toLocaleString('en-CA', {
+    timeZone: "America/Edmonton", year: "numeric", month: "short",
+    weekday: "long", day: "numeric"
+  });
+
+  const timeString = date.toLocaleString('en-CA', {
+      hour: "numeric", minute: "2-digit", second: "2-digit"
+  });
+
+  const timeNow = `${dateString} | ${timeString}`
 
   const time = document.querySelector(".time")
   time.innerHTML = `${timeNow}`
