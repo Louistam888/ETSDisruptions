3a6de0f6ef3e898e06bcffd8ea664f6aac39d6e3
Louistam888 <louistam888@gmail.com>
2023-01-24 16:32:51 -0500

    added luxon to other time objects

--- javascript/clock.js
+++ javascript/clock.js
@@ -2,11 +2,7 @@
 
 const refreshTime = () => {
 
-  let t = 0;
-
-  // const timeNow = (new Date).toLocaleString('en-CA', {timeZone: "America/Edmonton", year:"numeric", month:"short", weekday: "long", day:"numeric", hour12:true, hour: "numeric", minute:"2-digit", second: "2-digit"}).toString().replace(/,/g, match => ++t === 3 ? ' |' : match)
-  
-  const timeNow = luxon.DateTime.now().setZone("America/Edmonton").toFormat("cccc MMM d, yyyy | h:mm:ss a")
+  const timeNow = luxon.DateTime.now().setZone("America/Edmonton").toFormat("cccc, MMM d, yyyy | h:mm:ss a")
   
   const time = document.querySelector(".time")
   time.innerHTML = `${timeNow}`
@@ -20,13 +16,5 @@ setInterval(refreshTime, 1000)
 //FUNCTION FOR CONVERTING TIME OBJECTS
 
 const convertTime = (timeObj) => { 
-  return new Date(timeObj).toLocaleDateString("en-us", {
-    year: "numeric",
-    month: "short",
-    weekday: "short",
-    day: "numeric",
-    hour12: true,
-    hour: "numeric",
-    minute: "2-digit"
-  })
+  return luxon.DateTime.fromISO(timeObj).toFormat("ccc. MMM d, yyyy @ h:mm a")
 }
