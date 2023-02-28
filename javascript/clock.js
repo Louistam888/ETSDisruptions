5859ae0533e63c7ff9fbc507cdfd29c30a5defa6
Louistam888 <louistam888@gmail.com>
2023-02-28 18:09:34 -0500

    edits under clockjs

--- javascript/clock.js
+++ javascript/clock.js
@@ -1,11 +1,9 @@
 //FUNCTION FOR CLOCK IN HEADER 
 
 const refreshTime = () => {
-
   const timeNow = luxon.DateTime.now().setZone("America/Edmonton").toFormat("cccc, MMM d, yyyy | h:mm:ss a");
   const time = document.querySelector(".time");
   time.innerHTML = `${timeNow}`;
-
 }
 refreshTime();
 setInterval(refreshTime, 1000);
