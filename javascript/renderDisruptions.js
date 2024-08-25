855267375b7fa68a99c7327b472f63a4d3c71bbf
Louistam888 <louistam888@gmail.com>
2024-08-25 10:49:54 -0400

    fixed undefined condition

--- javascript/renderDisruptions.js
+++ javascript/renderDisruptions.js
@@ -12,7 +12,11 @@ const getEntry = (disruption, currentTimeUnix) =>
 
     if (window.location.pathname === "/currentDisruptions.html") {
       pageClass = ".serviceDisruptions";
-      if (currentTimeUnix >= disruptionStart && currentTimeUnix <= disruptionEnd && route !== undefined) {
+      if (
+        currentTimeUnix >= disruptionStart &&
+        currentTimeUnix <= disruptionEnd &&
+        route !== undefined
+      ) {
         return entry;
       }
     } else if (window.location.pathname === "/upcomingDisruptions.html") {
@@ -29,123 +33,142 @@ const renderDisruptions = async () => {
   const busStopsInfo = await fetchBusStopInfo();
   const disruptions = await fetchDisruptions();
 
-  const filteredArrayRaw = await getEntry(disruptions, currentTimeUnix);
-  const filteredArray = filteredArrayRaw
-    .sort((a, b) => {
-      if ((a.start_dttm ?? Number.MAX_VALUE) > (b.start_dttm ?? Number.MAX_VALUE)) return -1;
-      if ((a.start_dttm ?? Number.MAX_VALUE) < (b.start_dttm ?? Number.MAX_VALUE)) return 1;
-      return 0;
-    })
-    .reduce((accumulator, current) => {
-      if (
-        !accumulator.find(
-          (item) =>
-            item.description_text.replace(/ \nUse$/, "") === current.description_text.replace(/ \nUse$/, "") &&
-            item.stop_id === current.stop_id &&
-            item.route_id === current.route_id
+  if (disruptions !== undefined) {
+    const filteredArrayRaw = await getEntry(disruptions, currentTimeUnix);
+    const filteredArray = filteredArrayRaw
+      .sort((a, b) => {
+        if (
+          (a.start_dttm ?? Number.MAX_VALUE) >
+          (b.start_dttm ?? Number.MAX_VALUE)
         )
-      ) {
-        accumulator.push(current);
-      }
+          return -1;
+        if (
+          (a.start_dttm ?? Number.MAX_VALUE) <
+          (b.start_dttm ?? Number.MAX_VALUE)
+        )
+          return 1;
+        return 0;
+      })
+      .reduce((accumulator, current) => {
+        if (
+          !accumulator.find(
+            (item) =>
+              item.description_text.replace(/ \nUse$/, "") ===
+                current.description_text.replace(/ \nUse$/, "") &&
+              item.stop_id === current.stop_id &&
+              item.route_id === current.route_id
+          )
+        ) {
+          accumulator.push(current);
+        }
 
-      return accumulator;
-    }, [])
-    .sort((a, b) => {
-      if ((a.route_id ?? Number.MAX_VALUE) < (b.route_id ?? Number.MAX_VALUE)) return -1;
-      if ((a.route_id ?? Number.MAX_VALUE) > (b.route_id ?? Number.MAX_VALUE)) return 1;
-      return 0;
-    });
+        return accumulator;
+      }, [])
+      .sort((a, b) => {
+        if ((a.route_id ?? Number.MAX_VALUE) < (b.route_id ?? Number.MAX_VALUE))
+          return -1;
+        if ((a.route_id ?? Number.MAX_VALUE) > (b.route_id ?? Number.MAX_VALUE))
+          return 1;
+        return 0;
+      });
 
-  if (filteredArray.length === 0) {
-    document.querySelector(pageClass).replaceChildren();
+    if (filteredArray.length === 0) {
+      document.querySelector(pageClass).replaceChildren();
+      const newLi = document.createElement("li");
+      newLi.classList.add("apiError");
+      newLi.innerHTML = `There are no current service disruptions`;
+      document.querySelector(pageClass).append(newLi);
+    } else {
+      document.querySelector(pageClass).replaceChildren();
+
+      filteredArray.forEach((log) => {
+        const route = log.route_id;
+        const routeName = log.route_long_name;
+        const cause = cleanedCause(log.cause);
+        const effect = cleanedEffect(log.effect);
+        const shortDescription = log.header_text;
+        const start = convertTime(log.start_dttm);
+        const end = convertTime(log.end_dttm);
+        const stopArray = stopArrayTenary(log.stop_id);
+        const stopDetails = createStopString(stopArray, busStopsInfo);
+        const stopInfo = cleanedStopInfo(stopDetails);
+        const description = cleanedDescription(log.description_text);
+        const newLi = document.createElement("li");
+        newLi.classList.add("disruptionsHeader");
+        newLi.innerHTML = `<div class="accordion">
+            <div class="accordionItem">
+              <div class="accordionItemHeader">
+                ${route ? route : shortDescription}
+                ${routeName ? routeName : ""}
+                ${effect ? (effect === "UNKNOWN EFFECT" ? "" : effect) : ""}
+              </div>
+              <div class="accordionItemBody">
+                <div class="accordionContent">
+                  <div class="briefDescription">
+                    ${description ? description : "Information not available"}
+                  </div><!--briefDescription div end-->
+                  <div class="doubleInfoContainer">
+                      ${
+                        cause
+                          ? `<div class="doubleLeft">
+                          CAUSE
+                        </div><!--doubleLeft div end -->
+                        <div class="doubleRight">
+                          ${cause}
+                        </div>` // <!--doubleRight div end-->
+                          : "No cause specified"
+                      }
+                  </div><!--doubleInfoContainer div end -->
+                  <div class="doubleInfoContainer">
+                    ${
+                      start
+                        ? `<div class="doubleLeft">
+                          DISRUPTION START
+                        </div><!--doubleLeft div end -->
+                        <div class="doubleRight">
+                          ${start}
+                        </div>` // <!--doubleRight div end-->
+                        : "No disruption start time specifed"
+                    }
+                  </div><!--doubleInfoContainer div end -->
+                  <div class="doubleInfoContainer">
+                    ${
+                      end
+                        ? `<div class="doubleLeft">
+                          DISRUPTION END
+                        </div><!--doubleLeft div end -->
+                        <div class="doubleRight">
+                          ${end}
+                        </div>` // <!--doubleRight div end-->
+                        : "No disruption end time specifed"
+                    }
+                  </div><!--doubleInfoContainer div end -->
+                  <div class="doubleInfoContainer">            
+                    <div class="doubleLeft">
+                      STOP LOCATION(S)
+                    </div><!--doubleRight div end-->
+                    <div class="doubleRight stop">
+                      ${stopInfo}
+                    </div><!--doubleRight stop div end-->
+                  </div><!--doubleInfoContainer div end -->
+                </div><!--accordionContent div end -->
+              </div> <!--accordionitembody div end-->
+            </div><!--accordionItem div end-->
+          </div><!--accordion div end-->`;
+        document.querySelector(pageClass).append(newLi);
+      });
+      sameHeights(false);
+      window.onresize = () => {
+        sameHeights(true);
+      };
+      disruptionAccordions();
+    }
+  } else {
+    document.querySelector(".serviceDisruptions").replaceChildren();
     const newLi = document.createElement("li");
     newLi.classList.add("apiError");
-    newLi.innerHTML = `There are no current service disruptions`;
-    document.querySelector(pageClass).append(newLi);
-  } else {
-    document.querySelector(pageClass).replaceChildren();
-
-    filteredArray.forEach((log) => {
-      const route = log.route_id;
-      const routeName = log.route_long_name;
-      const cause = cleanedCause(log.cause);
-      const effect = cleanedEffect(log.effect);
-      const shortDescription = log.header_text;
-      const start = convertTime(log.start_dttm);
-      const end = convertTime(log.end_dttm);
-      const stopArray = stopArrayTenary(log.stop_id);
-      const stopDetails = createStopString(stopArray, busStopsInfo);
-      const stopInfo = cleanedStopInfo(stopDetails);
-      const description = cleanedDescription(log.description_text);
-      const newLi = document.createElement("li");
-      newLi.classList.add("disruptionsHeader");
-      newLi.innerHTML = `<div class="accordion">
-          <div class="accordionItem">
-            <div class="accordionItemHeader">
-              ${route ? route : shortDescription}
-              ${routeName ? routeName : ""}
-              ${effect ? (effect === "UNKNOWN EFFECT" ? "" : effect) : ""}
-            </div>
-            <div class="accordionItemBody">
-              <div class="accordionContent">
-                <div class="briefDescription">
-                  ${description ? description : "Information not available"}
-                </div><!--briefDescription div end-->
-                <div class="doubleInfoContainer">
-                    ${
-                      cause
-                        ? `<div class="doubleLeft">
-                        CAUSE
-                      </div><!--doubleLeft div end -->
-                      <div class="doubleRight">
-                        ${cause}
-                      </div>` // <!--doubleRight div end-->
-                        : "No cause specified"
-                    }
-                </div><!--doubleInfoContainer div end -->
-                <div class="doubleInfoContainer">
-                  ${
-                    start
-                      ? `<div class="doubleLeft">
-                        DISRUPTION START
-                      </div><!--doubleLeft div end -->
-                      <div class="doubleRight">
-                        ${start}
-                      </div>` // <!--doubleRight div end-->
-                      : "No disruption start time specifed"
-                  }
-                </div><!--doubleInfoContainer div end -->
-                <div class="doubleInfoContainer">
-                  ${
-                    end
-                      ? `<div class="doubleLeft">
-                        DISRUPTION END
-                      </div><!--doubleLeft div end -->
-                      <div class="doubleRight">
-                        ${end}
-                      </div>` // <!--doubleRight div end-->
-                      : "No disruption end time specifed"
-                  }
-                </div><!--doubleInfoContainer div end -->
-                <div class="doubleInfoContainer">            
-                  <div class="doubleLeft">
-                    STOP LOCATION(S)
-                  </div><!--doubleRight div end-->
-                  <div class="doubleRight stop">
-                    ${stopInfo}
-                  </div><!--doubleRight stop div end-->
-                </div><!--doubleInfoContainer div end -->
-              </div><!--accordionContent div end -->
-            </div> <!--accordionitembody div end-->
-          </div><!--accordionItem div end-->
-        </div><!--accordion div end-->`;
-      document.querySelector(pageClass).append(newLi);
-    });
-    sameHeights(false);
-    window.onresize = () => {
-      sameHeights(true);
-    };
-    disruptionAccordions();
+    newLi.innerHTML = `This City of Edmonton API is undergoing temporary maintenance. This issue should be resolved soon. Please check back later`;
+    document.querySelector(".serviceDisruptions").append(newLi);
   }
 };
 
